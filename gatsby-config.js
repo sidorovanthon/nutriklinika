3bbedc9797f8d3cb59da562541190621477c6a41
sidorovanthon <sidorov.anthon@gmail.com>
2020-10-30 08:20:57 +0300

    Add prismic generated pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
         accessToken: 'MC5YNWtzU0JBQUFDSUFoSnBi.77-977-977-9aShO77-977-977-9C--_vS_vv71uSu-_vTNG77-977-9eu-_ve-_ve-_vXhN77-9Vmvvv73vv70r',
         schemas: {
           articles: require('./src/schemas/article.json'),
+          homepage: require('./src/schemas/homepage.json'),
         },
         lang: '*',
         prismicToolbar: true,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions;
 
@@ -6,3 +8,37 @@ exports.onCreatePage = ({ page, actions }) => {
     createPage(page);
   }
 };
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const pages = await graphql(`
+    {
+      allPrismicArticle {
+        nodes {
+          id
+          uid
+          data {
+            title {
+              text
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const pageTemplates = {
+    Article: path.resolve(__dirname, 'src/templates/article.js'),
+  }
+
+  pages.data.allPrismicArticle.nodes.forEach((node) => {
+    createPage({
+      path: `/${node.uid}`,
+      component: pageTemplates.Article,
+      context: {
+        id: node.id,
+      },
+    })    
+  })
+}
